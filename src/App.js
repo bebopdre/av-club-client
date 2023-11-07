ea33978b4566120ef8193b92d9374fa11a29e5cf
dre <andreadrematos@gmail.com>
2023-11-06 22:28:01 -0500

    getting everything on the page and marginal styling

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import HomePage from "./pages/HomePage/HomePage";
 import './App.scss';
+import PickAvatar from "./pages/PickAvatar/PickAvatar";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<HomePage />} />
+        <Route path='/pick-avatar' element={<PickAvatar />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/PickAvatar/PickAvatar.js
+++ src/pages/PickAvatar/PickAvatar.js
@@ -1,8 +1,42 @@
 import './PickAvatar.scss';
 
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
 function PickAvatar() {
+    const [avatars, setAvatars] = useState([]);
+
+    useEffect( () => {
+
+        async function getAvatars() {
+            const response = await axios.get('http://localhost:8080/avatars');
+            setAvatars(response.data);
+        }
+        getAvatars();
+
+    }, []);
+
+
     return (
-        <main>
+        <main className='pick-avatar'>
+
+            <h2 className='pick-avatar__heading'>
+                PICK AN AVATAR
+            </h2>
+
+            {
+                avatars &&
+                <ul className='pick-avatar__avatars'>
+                    {
+                        avatars.map( avatar => (
+                            <li key={avatar.id} className='pick-avatar__avatars-avatar'>
+                                <img src={avatar.avatar} alt='avatar' className='pick-avatar__avatars-image'></img>
+                            </li>
+                        ))
+                    }
+                </ul>
+            }
+            
         </main>
     )
 }
